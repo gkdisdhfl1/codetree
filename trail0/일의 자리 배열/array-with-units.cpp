7181e5792b437373e37fa86339e20e2b6b97610e
gkdisdhfl1 <67906779+gkdisdhfl1@users.noreply.github.com>
2026-05-18 02:00:53 +0900

    [Passed] 일의 자리 배열 (8ms, 0MB)

--- trail0/일의 자리 배열/array-with-units.cpp
+++ trail0/일의 자리 배열/array-with-units.cpp
@@ -8,10 +8,7 @@ int main() {
     cin >> a >> b;
     v[0] = a; v[1] = b;
     for(int i = 2; i < 10; ++i) {
-        int n = v[i - 2] + v[i - 1];
-        if(n >= 10)
-            n -= 10;
-        v[i] = n;
+        v[i] = (v[i - 2] + v[i - 1]) % 10;
     }
 
     for(int n : v) {
